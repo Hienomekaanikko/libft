575dadb7bf543980efb5e36fb3cba2aecf375e3e
Mikko Suokas <msuokas@c1r4p8.hive.fi>
2024-11-20 15:45:53 +0200

    added free malloc in case of failure

--- ft_split.c
+++ ft_split.c
@@ -3,59 +3,86 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msuokas <msuokas@student.42.fr>            +#+  +:+       +#+        */
+/*   By: msuokas <msuokas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/01 15:42:11 by msuokas           #+#    #+#             */
-/*   Updated: 2024/11/01 17:09:44 by msuokas          ###   ########.fr       */
+/*   Created: 2024/11/07 16:15:16 by msuokas           #+#    #+#             */
+/*   Updated: 2024/11/20 09:39:21 by msuokas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+static int	ft_count_splits(char const *s, char c)
 {
-	char	**array_of_strings;
-	int	i;
-	int y;
-	int	splits;
+	int	count;
+	int	in_word;
+
+	count = 0;
+	in_word = 0;
+	while (*s)
+	{
+		if (*s != c && !in_word)
+		{
+			in_word = 1;
+			count++;
+		}
+		else if (*s == c)
+			in_word = 0;
+		s++;
+	}
+	return (count);
+}
+
+static char	**free_malloc(char **array_of_strings)
+{
+	size_t	i;
 
 	i = 0;
-	splits = 1;
-	while (s[i] != '\0')
+	while (array_of_strings[i])
 	{
-		if (s[i] == c)
-			splits++;
+		free(array_of_strings[i]);
 		i++;
 	}
-	array_of_strings = malloc(splits * sizeof(char*));
-	if (array_of_strings == NULL)
-		return (0);
+	free(array_of_strings);
+	return (NULL);
+}
+
+static char	**split_the_strings(char const *s, char c, char **array_of_strings)
+{
+	int	i;
+	int	j;
+	int	y;
+
 	i = 0;
 	y = 0;
-	while (s[i] != '\0')
+	while (s[i])
 	{
-		if (s[i] == c)
-			y++;
-		else
-			array_of_strings[y][i] = s[i];
-		i++;
+		while (s[i] == c)
+			i++;
+		if (!s[i])
+			break ;
+		j = i;
+		while (s[i] && s[i] != c)
+			i++;
+		array_of_strings[y] = ft_substr(s, j, i - j);
+		if (!array_of_strings[y])
+			return (free_malloc(array_of_strings));
+		y++;
 	}
+	array_of_strings[y] = NULL;
 	return (array_of_strings);
 }
 
-int	main(void)
+char	**ft_split(char const *s, char c)
 {
-	int	i;
-	char	str[] = "HELLOO5MITAA5KUUULUUU5MITAMITA";
-	char	**str1;
+	char	**array_of_strings;
+	int		amount_of_strings;
 
-	i = 0;
-	printf("%s\n", str);
-	str1 = ft_split(str, '5');
-	while (*str1[i] != '\0')
-	{
-		printf("%s", str1[i]);
-		i++;
-	}
-
+	if (!s)
+		return (NULL);
+	amount_of_strings = ft_count_splits(s, c);
+	array_of_strings = malloc((amount_of_strings + 1) * sizeof(char *));
+	if (array_of_strings == NULL)
+		return (NULL);
+	return (split_the_strings(s, c, array_of_strings));
 }
